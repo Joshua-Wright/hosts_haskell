3c8d1fcf25f83cd00b12932dcfeacc9fb1103f59
Josh Wright <madewithlinux.josh@gmail.com>
2017-07-04 14:57:56 -0400

    style cleanup

--- src/Main.hs
+++ src/Main.hs
@@ -3,8 +3,8 @@
 module Main where
 import           Control.Concurrent.ParallelIO.Global
 import           Control.Exception
-import qualified Data.ByteString                      as B
 import qualified Data.ByteString.Lazy                 as BL
+import           Data.Either.Combinators
 import           Data.List
 import           Data.Maybe
 import qualified Data.Set                             as S
@@ -14,28 +14,32 @@ import qualified Data.Text.IO                         as T
 import           Network.HTTP.Conduit
 import           System.Console.GetOpt
 import           System.Directory
-import           System.FilePath
 import           System.Environment
-import Data.Either.Combinators
-import System.ProgressBar
+import           System.FilePath
+import           System.ProgressBar
 
+hostnamePath :: FilePath
 hostnamePath = "/etc/hostname"
-makeHeader hostname = T.concat
+
+makeHeader :: T.Text -> T.Text
+makeHeader currentHostname = T.concat
     [ "# hosts file made by j0sh\n"
-    , "127.0.0.1 ", hostname, " # current hostname for this machine\n"
-    , "::1 ", hostname, " # current hostname for this machine\n"
+    , "127.0.0.1 ", currentHostname, " # current hostname for this machine\n"
+    , "::1 ", currentHostname, " # current hostname for this machine\n"
     , "127.0.0.1 localhost # IPv4 localhost\n"
     , "::1 localhost #IPv6 localhost\n\n"
     ]
+
+linePrefix :: T.Text
 linePrefix = "0.0.0.0 "
 
 data Options = Options
     { urls      :: IO [String]
     , whitelist :: IO [String]
     , hostname  :: IO T.Text
-    , output    :: (T.Text -> IO ())
+    , output    :: T.Text -> IO ()
     }
-defaultOptions    = Options
+defaultOptions  = Options
     { urls      = return []
     , whitelist = getDefaultWhitelist
     , hostname  = getHostName
@@ -51,16 +55,16 @@ outputFile path = T.writeFile path
 options :: [OptDescr (Options -> Options)]
 options =
     [ Option ['i'] ["input"]
-        (ReqArg (\path opts -> opts{urls=(readFileLines path)}) "input")
+        (ReqArg (\path opts -> opts{urls=(readFileLines path)}) "INPUT")
         "input file"
     , Option ['o'] ["output"]
         (ReqArg (\path opts -> opts{output=(outputFile path)}) "OUTPUT" )
         "output file. Defaults to stdout"
     , Option ['w'] ["whitelist"]
-        (ReqArg (\path opts -> opts{whitelist=(readFileLines path)}) "whitelist")
+        (ReqArg (\path opts -> opts{whitelist=(readFileLines path)}) "WHITELIST")
         "whitelist file. Defaults to ~/.hosts_whitelist.txt"
     , Option ['h'] ["hostname"]
-        (ReqArg (\host opts -> opts{hostname=(return . T.pack $ host)}) "host")
+        (ReqArg (\host opts -> opts{hostname=(return . T.pack $ host)}) "HOSTNAME")
         "hostname to use. Tries to determine system hostname if unspecified"
     ]
 
@@ -81,7 +85,7 @@ processArgs args = do
     (bar, _)  <- startProgress (msg "Downloading") exact 80 $ fromIntegral $ length urls
     -- we use extraWorkerWhileBlocked to create extra threads to do work while
     -- existing ones are blocking on file downloads
-    sets      <- parallel $ map extraWorkerWhileBlocked $ map  (downloadHostsFile bar) $ urls
+    sets      <- parallel $ map (extraWorkerWhileBlocked . downloadHostsFile bar) urls
     putStrLn ""
     putStrLn "Joining files and writing output..."
     let finalSet     = S.unions sets
@@ -90,7 +94,8 @@ processArgs args = do
         header       = makeHeader hostname
     output opts $ T.append header $ T.unlines outputLines
 
-main = do getArgs >>= processArgs
+main :: IO ()
+main = getArgs >>= processArgs
 
 
 filterLine :: T.Text -> Maybe T.Text
@@ -104,31 +109,32 @@ addPrefix l = T.append linePrefix $ T.strip $ T.takeWhile (/= '#') l
 downloadHostsFile :: ProgressRef -> String -> IO (S.Set T.Text)
 downloadHostsFile bar url = do
     fileContent <- catch (simpleHttp url)
-                         (\e  -> do
-                            incProgress bar 1
-                            let msg = show (e :: HttpException)
-                            putStrLn $ "Failed to download: " ++ url
-                            return "")
+                         nopHandler
     incProgress bar 1
     let eitherText = T.decodeUtf8' $ BL.toStrict fileContent
         text = fromRight "" eitherText
         outputSet = S.fromList $ mapMaybe filterLine $ T.lines text
-    return (outputSet)
+    return outputSet
+  where
+    nopHandler :: HttpException -> IO BL.ByteString
+    nopHandler e = do
+      incProgress bar 1
+      return ""
 
 filterComments :: [String] -> [String]
 filterComments = mapMaybe strFilter
     where
         strFilter :: String -> Maybe String
         strFilter x
-            | isPrefixOf "#"  x = Nothing
-            | x == ""           = Nothing
-            | otherwise         = Just x
+            | "#" `isPrefixOf` x = Nothing
+            | x == ""            = Nothing
+            | otherwise           = Just x
 
 getHostName :: IO T.Text
 getHostName = do
     hostnameFileExists <- doesFileExist (hostnamePath :: FilePath)
-    if (hostnameFileExists) then
-        do (T.readFile hostnamePath >>= return . T.strip)
+    if hostnameFileExists then
+        fmap T.strip (T.readFile hostnamePath)
     else
         return "localhost"
 
@@ -137,7 +143,7 @@ getDefaultWhitelist = do
     homeFolder <- getHomeDirectory
     let whitelistPath = homeFolder </> (".hosts_whitelist.txt" :: FilePath)
     fileExists <- doesFileExist (whitelistPath :: FilePath)
-    if (fileExists) then do
+    if fileExists then do
         filecontents <- readFile whitelistPath
         return (lines filecontents)
     else
